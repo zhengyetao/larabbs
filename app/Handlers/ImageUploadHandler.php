e3e0d9f7659ca85036193ffdb0ae99e549f3981e
vagrant <vagrant@homestead>
2019-09-20 15:16:16 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -3,12 +3,13 @@
 namespace App\Handlers;
 
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler
 {
     protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
@@ -24,8 +25,24 @@ class ImageUploadHandler
 
         $file->move($upload_path, $filename);
 
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 }
